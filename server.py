769d7b1e3af5fe288e4ede13f4964cc8f3738ec2
Amit Sharma <40901820+amshar05@users.noreply.github.com>
2021-06-06 17:25:52 +0530

    added df for excel

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import redirect,  url_for, render_template,request,Flask,Response,json,send_file
+import pandas as pd
 
-
-
+import datetime
 
 
 
@@ -11,7 +11,10 @@ app = Flask(__name__)
 @app.route("/haryana/",methods=['GET','POST'])
 def haryana():
 	district = ['Ambala', 'Bhiwani', 'Charkhi Dadri', 'Faridabad', 'Fatehabad', 'Gurugram', 'Hisar', 'Jind', 'Kaithal', 'Karnal', 'Kurukshetra', 'Nuh', 'Palwal', 'Panchkula', 'Panipat', 'Rohtak', 'Sirsa', 'Sonipat', 'Yamunanagar', 'Jhajjar', 'Mahendragarh', 'Rewari']
-	return render_template("haryana.html", district= district)
+	df = pd.read_excel('data_info.xlsx')
+	df[0] = df[0].dt.date
+	df.columns = ['district','date','confirmed','recovered','deceased']
+	return render_template("haryana.html", district= district, df = df)
 
 if __name__ == '__main__':
     app.debug = True
